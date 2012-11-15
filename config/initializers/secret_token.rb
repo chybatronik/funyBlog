2faebcefc90988017469687c6d6570eacdb1df59
Chybatronik <chybatron@gmail.com>
2012-11-15 11:22:06 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunyBlog::Application.config.secret_token = '51f4d07312ed4754f8c978642c7d26e8c6f5be0427c55a6ba01fdcf4df5c1073ed06dd16817da3aaa819fdc1fc11619a6437491d71cd69edc3ec828072b317f2'
